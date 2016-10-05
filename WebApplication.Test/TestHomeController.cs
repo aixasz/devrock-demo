582b4a08068b0950a4ca2b2d0d3bcbeecee49be9
Nattapong Nunpan <aixasz@hotmail.com>
2016-10-05 21:32:32 +0700

    Add unit test for home controller

---
+++ WebApplication.Test/TestHomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Controllers;
+using Xunit;
+
+namespace WebApplication.Test
+{
+
+    public class TestHomeController
+    { 
+        [Fact]
+        public void TestHomeController_HaveViewBagMessage()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+            Assert.Equal("Hello World", controller.ViewBag.Message);
+        }
+    }
+}
